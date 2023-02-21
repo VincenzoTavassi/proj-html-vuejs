883d53bf3dccf00ceba1331e9b48b7535e2d872c
VincenzoTavassi <info@egadivacanze.it>
2023-02-21 18:09:11 +0100

    full html css js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            message: 'Hello Vue!'
-        }
-    }
-}).mount('#root')
+  data() {
+    return {
+      headerLinks: [
+        { text: "home", active: true, link: "#" },
+        { text: "shop", active: false, link: "#" },
+        { text: "about", active: false, link: "#" },
+        { text: "gallery", active: false, link: "#" },
+        { text: "locations", active: false, link: "#locations" },
+        { text: "journal", active: false, link: "#" },
+        { text: "contacts", active: false, link: "#" },
+        { text: "my account", active: false, link: "#" },
+      ],
+      footerLinks: [
+        { text: "home", active: false, link: "#" },
+        { text: "shop", active: false, link: "#" },
+        { text: "about", active: false, link: "#" },
+        { text: "gallery", active: false, link: "#" },
+        { text: "locations", active: false, link: "#locations" },
+        { text: "journal", active: false, link: "#" },
+        { text: "contacts", active: false, link: "#" },
+        { text: "orders", active: false, link: "#" },
+      ],
+      orderCount: 2,
+    };
+  },
+}).mount("#root");
 
-console.log(Vue)
+console.log(Vue);
