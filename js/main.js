7fbd9a89b8513ae0be040fda36f7d8b17243544b
VincenzoTavassi <info@egadivacanze.it>
2023-02-22 17:14:34 +0100

    sliders

--- js/main.js
+++ js/main.js
@@ -23,10 +23,118 @@ createApp({
         { text: "contacts", active: false, link: "#" },
         { text: "orders", active: false, link: "#" },
       ],
+      products: [
+        {
+          text: "Choco Chips Cookies",
+          category: "cookies, pastries",
+          price: "$19.00 - $39.00",
+          img: "./img/choco-chip-cookies.jpg",
+        },
+        {
+          text: "Strawberry Jam Cookies",
+          category: "cookies, pastries",
+          price: "$24.00 - $62.00",
+          img: "./img/strawberry-jam-cookies.jpg",
+        },
+        {
+          text: "Strawberry Donut",
+          category: "pastries",
+          price: "$24.00 - $42.00",
+          img: "./img/strawberry-donut.jpg",
+        },
+        {
+          text: "Perfect Macarons",
+          category: "cookies",
+          price: "$18.00 - $52.00",
+          img: "./img/perfect-macarons.jpg",
+        },
+        {
+          text: "Glazed Pancake",
+          category: "pastries",
+          price: "$28.00 - $56.00",
+          img: "./img/glazed-pancake-with-lemon.jpg",
+        },
+        {
+          text: "Cookies with ice cream",
+          category: "pastries - cookies",
+          price: "$21.00 - $36.00",
+          img: "./img/cookies-with-ice-cream.jpg",
+        },
+      ],
       logo: "./img/avada-bakery-logo.png",
       orderCount: 2,
+      activeProducts: [0, 1],
+      productSlider: [0, 1, 2, 3],
     };
   },
+  computed: {
+    firstTopSlide() {
+      return this.products[this.activeProducts[0]];
+    },
+    secondTopSlide() {
+      return this.products[this.activeProducts[1]];
+    },
+    slideZero() {
+      return this.products[this.productSlider[0]];
+    },
+    slideOne() {
+      return this.products[this.productSlider[1]];
+    },
+    slideTwo() {
+      return this.products[this.productSlider[2]];
+    },
+    slideThree() {
+      return this.products[this.productSlider[3]];
+    },
+  },
+  methods: {
+    moveForward() {
+      if (this.activeProducts[1] + 1 < this.products.length) {
+        this.activeProducts[0] = this.activeProducts[1];
+        this.activeProducts[1]++;
+      } else {
+        this.activeProducts[0] = this.products.length - 1;
+        this.activeProducts[1] = 0;
+      }
+    },
+    moveBack() {
+      if (this.activeProducts[0] - 1 < 0) {
+        this.activeProducts[0] = this.products.length - 1;
+        this.activeProducts[1] = 0;
+      } else {
+        this.activeProducts[1] = this.activeProducts[0];
+        this.activeProducts[0]--;
+      }
+    },
+    sliderForward() {
+      if (this.productSlider[3] + 1 < this.products.length) {
+        this.productSlider[0] = this.productSlider[1];
+        this.productSlider[1]++;
+        this.productSlider[1] = this.productSlider[2];
+        this.productSlider[2]++;
+        this.productSlider[2] = this.productSlider[3];
+        this.productSlider[3]++;
+      } else {
+        this.productSlider[3] = 0;
+        this.productSlider[2] = this.products.length - 1;
+        this.productSlider[1] = this.products.length - 2;
+        this.productSlider[0] = this.products.length - 3;
+      }
+    },
+    sliderBack() {
+      if (this.productSlider[0] - 1 >= 0) {
+        this.productSlider[3] = this.productSlider[2];
+        this.productSlider[2]--;
+        this.productSlider[2] = this.productSlider[1];
+        this.productSlider[1]--;
+        this.productSlider[1] = this.productSlider[0];
+        this.productSlider[0]--;
+      } else {
+        this.productSlider[0] = this.products.length - 1;
+        this.productSlider[1] = 0;
+        this.productSlider[2] = 1;
+        this.productSlider[3] = 2;
+      }
+    },
+  },
 }).mount("#root");
-
-console.log(Vue);
